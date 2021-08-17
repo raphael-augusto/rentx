b4c29b78565a26fd9db4a7463a4a71c1190f5838
Raphael Augusto <raphaelaugusto@tutanota.com>
2021-08-16 21:52:41 -0300

    feat: add ImageSlider components

---
+++ src/components/ImageSlider/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import * as S from './styles';
+
+interface Props {
+  imagesUrl: string[];
+}
+
+export function ImageSlider({ imagesUrl }:Props){
+  return (
+    <S.Container>
+      <S.ImageIndexs>
+        <S.ImageIndex active={true}/>
+        <S.ImageIndex active={false}/>
+        <S.ImageIndex active={false}/>
+        <S.ImageIndex active={false}/>
+        <S.ImageIndex active={false}/>
+      </S.ImageIndexs>
+      <S.CarImageWrapper>
+        <S.CarImage
+          source={{uri: imagesUrl[0]}}
+          resizeMode="contain"
+        />
+
+      </S.CarImageWrapper>
+
+    </S.Container>
+  );
+};
