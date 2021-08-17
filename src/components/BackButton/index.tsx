76f6c108c4d513a2d1e172100de94a87b778202e
Raphael Augusto <raphaelaugusto@tutanota.com>
2021-08-16 21:52:12 -0300

    feat: add BackButton components

---
+++ src/components/BackButton/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useTheme } from 'styled-components';
+import { BorderlessButton } from 'react-native-gesture-handler';
+
+import * as S from './styles';
+
+interface Props extends BorderlessButton {
+  color?: string;
+  size?: number;
+}
+
+export function BackButton({ color, size , ...rest }: Props){
+  const theme = useTheme();
+
+  return (
+    <S.Container {...rest}>
+      <MaterialIcons
+        name="chevron-left"
+        size={size ? size : 24}
+        color={color ? color : theme.colors.text}
+      />
+
+    </S.Container>
+  );
+};
