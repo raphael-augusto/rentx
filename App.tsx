50deab67dbfedae7b3c254f352aac6eda839e9f8
Raphael Augusto <raphaelaugusto@tutanota.com>
2021-08-23 00:42:23 -0300

    feat: add Routes

--- App.tsx
+++ App.tsx
@@ -12,8 +12,8 @@ import {
   Archivo_600SemiBold
 } from '@expo-google-fonts/archivo';
 
+import { Routes } from './src/routes';
 
-import { Home } from './src/screens/Home';
 import theme from './src/styles/theme';
 
 
@@ -30,7 +30,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <Routes />
     </ThemeProvider>
   );
 }
