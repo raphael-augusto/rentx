d7e9bba0f1ec4488b1f8399f418e8f34c913bce4
Raphael Augusto <raphaelaugusto@tutanota.com>
2021-10-13 00:15:32 -0300

    fix: add loading

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
+import { useTheme } from 'styled-components';
 
 import * as S from './styles';
 
@@ -6,15 +8,31 @@ interface Props {
   title: string;
   color?: string;
   onPress: () => void;
+  enabled?: boolean;
+  loading?: boolean;
 }
 
-export function Button({title, color, onPress}:Props){
+export function Button({
+  title,
+  color,
+  onPress,
+  enabled = true,
+  loading = false
+}: Props){
+  const theme = useTheme();
+
   return (
     <S.Container
-      color={color}
+      color={color ? color : theme.colors.main}
       onPress={onPress}
+      enabled={enabled}
+      style={{ opacity: (enabled === false || loading === true) ? .5 : 1 }}
     >
-      <S.Title>{title}</S.Title>
+      {
+        loading
+        ? <ActivityIndicator color={theme.colors.shape} />
+        : <S.Title>{title}</S.Title>
+      }
     </S.Container>
   );
 };
