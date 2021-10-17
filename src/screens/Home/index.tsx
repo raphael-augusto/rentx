bed6cb4f6a4add88341d4de896fe1c372ff7820a
Raphael Augusto <raphaelaugusto@tutanota.com>
2021-10-17 00:06:16 -0300

    feat:  animation button my car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,26 @@
 import React, { useEffect, useState }from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { StatusBar } from 'react-native';
+import { StatusBar, StyleSheet, BackHandler } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useTheme } from 'styled-components';
+import Animated,{
+  useSharedValue,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+  withSpring
+} from 'react-native-reanimated';
+import { RectButton, PanGestureHandler } from 'react-native-gesture-handler';
 
 import { api } from '../../services/api';
 import { CarDTO } from '../../dtos/CarDTO';
 import { Header } from '../../components/Header';
 import { Car } from '../../components/Car';
 import { Load } from '../../components/Load';
-import {ConfirmButton} from '../../components/ConfirmButton'
 
 import * as S from './styles';
+
+/** Component Button My Cars */
+const ButtonAnimated = Animated.createAnimatedComponent(RectButton);
 
 interface Params {
   car: CarDTO;
@@ -26,15 +35,42 @@ export function Home(){
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [loading, setLoading] = useState(true);
   const navigation = useNavigation<NavigationProps>();
-  const theme = useTheme()
+  const theme = useTheme();
+
+  const positionY = useSharedValue(0);
+  const positionX = useSharedValue(0);
+
+  const myCarButtonStyle = useAnimatedStyle(() => {
+    return{
+      transform: [
+        { translateX: positionX.value },
+        { translateY: positionY.value },
+      ],
+    }
+  });
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart( _, ctx: any){
+      ctx.positionX = positionX.value;
+      ctx.positionY = positionY.value;
+    },
+    onActive(event, ctx: any){
+      positionX.value = ctx.positionX + event.translationX;
+      positionY.value = ctx.positionY + event.translationY;
+    },
+    onEnd(){
+      positionX.value = withSpring(0);
+      positionY.value = withSpring(0);
+    }
+  });
 
   function handleCarDetails(car: CarDTO) {
     navigation.navigate('CardDetails', { car });
-  }
+  };
 
   function handleMyCars() {
     navigation.navigate('MyCars');
-  }
+  };
 
   useEffect(()=>{
     const fetchCars = async () => {
@@ -52,6 +88,13 @@ export function Home(){
     fetchCars();
   },[]);
 
+  /** Don't go back to the home screen Android*/
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', () =>{
+      return true;
+    })
+  },[]);
+
   return (
     <S.Container >
       <StatusBar
@@ -60,25 +103,51 @@ export function Home(){
         backgroundColor="transparent"
         translucent
       />
-      <Header title={`Total de ${cars.length} carros`} />
-      {loading
-      ? <Load />
-      : <S.CardList
-          data={cars}
-          keyExtractor={item => item.id}
-          renderItem={({item}) =>
-            <Car data={item} onPress={()=>handleCarDetails(item)}/>
-          }
-        />
-      }
 
-      <S.MyCarsButton onPress={handleMyCars}>
-        <Ionicons
-          name="ios-car-sport"
-          size={32}
-          color={theme.colors.shape}
-        />
-      </S.MyCarsButton>
+      { !loading && <Header title={`Total de ${cars.length} carros`} /> }
+
+      {
+        loading
+        ? <Load />
+        : <S.CardList
+            data={cars}
+            keyExtractor={item => item.id}
+            renderItem={({item}) =>
+              <Car data={item} onPress={()=>handleCarDetails(item)}/>
+            }
+          />
+      }
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View style={[
+          myCarButtonStyle,
+          {
+            position: 'absolute',
+            bottom: 13,
+            right: 22
+          }
+        ]}>
+          <ButtonAnimated
+            onPress={handleMyCars}
+            style={[styles.button, {backgroundColor: theme.colors.main}]}
+          >
+            <Ionicons
+              name="ios-car-sport"
+              size={32}
+              color={theme.colors.shape}
+            />
+          </ButtonAnimated>
+        </Animated.View>
+      </PanGestureHandler>
     </S.Container>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
