9b4fd4e739243ec2d3af82c46de16f1cbccc6d44
Raphael Augusto <raphaelaugusto@tutanota.com>
2021-10-24 00:05:49 -0300

    feat: add SignUpFirstStep and SignUpSecondStep

--- src/routes/RootStackParams.ts
+++ src/routes/RootStackParams.ts
@@ -1,12 +1,17 @@
 import { CarDTO } from '../dtos/CarDTO';
+import { UserDTO } from '../dtos/UserDTO';
+import { ConfirmationDTO } from '../dtos/ConfirmationDTO';
+
 
 export type RootStackParams={
   Splash: undefined;
   SignIn: undefined;
+  SignUpFirstStep: undefined;
+  SignUpSecondStep: { userDTO: UserDTO };
   Home: undefined;
   CardDetails: { carDTO: CarDTO };
   Scheduling: { carDTO: CarDTO };
-  SchedulingDetails: {carDTO: CarDTO; dates:string[]};
-  SchedulingComplete?: {carDTO: CarDTO; dates:string[]};
+  SchedulingDetails: { carDTO: CarDTO; dates:string[]};
+  Confirmation: { confirmationDTO: ConfirmationDTO };
   MyCars: undefined;
 }
