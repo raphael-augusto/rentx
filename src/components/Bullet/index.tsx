4ad91e9cbbb067e7afd1fa3dd9d8629800043ac9
Raphael Augusto <raphaelaugusto@tutanota.com>
2021-10-24 00:07:07 -0300

    feat: add component  Bullet

---
+++ src/components/Bullet/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+interface BulletProps {
+  active?: boolean;
+};
+
+export function Bullet({ active = false }:BulletProps){
+  return (
+    <S.Container active={active} />
+  );
+};
