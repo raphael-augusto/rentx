f6fd199796e2cadb9aa9f659da2a3d2b96aa8b15
Raphael Augusto <raphaelaugusto@tutanota.com>
2021-11-14 23:55:17 -0300

    refactor: function and navigation

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState }from 'react';
 import {  useNavigation } from '@react-navigation/native';
-import { StatusBar,  BackHandler, Alert } from 'react-native';
+import { StatusBar,  BackHandler} from 'react-native';
 import { useNetInfo } from '@react-native-community/netinfo';
+import { synchronize } from '@nozbe/watermelondb/sync';
+import { database } from '../../database';
 
 import { api } from '../../services/api';
-import { CarDTO } from '../../dtos/CarDTO';
 import { Header } from '../../components/Header';
 import { Car } from '../../components/Car';
 import { Load } from '../../components/Load';
+import { Car as ModelCar} from '../../database/model/Car';
 
 
 import * as S from './styles';
@@ -16,24 +18,41 @@ import * as S from './styles';
 //const ButtonAnimated = Animated.createAnimatedComponent(RectButton);
 
 export function Home(){
-  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [cars, setCars] = useState<ModelCar[]>([]);
   const [loading, setLoading] = useState(true);
   const netInfo = useNetInfo();
   const navigation = useNavigation();
 
-  function handleCarDetails(car: CarDTO) {
-    navigation.navigate('CardDetails', { carDTO: car });
+  async function handleCarDetails(carId: string) {
+    navigation.navigate('CardDetails', {carId});
   };
+
+  async function offlineSynchronize() {
+    await synchronize({
+      database,
+      pullChanges: async ({ lastPulledAt }) => {
+        const response = await api.get(`cars/sync/pull?lastPulledVersion=${lastPulledAt || 0}`);
+        const { changes, latestVersion } = response.data;
+
+        return { changes, timestamp:latestVersion };
+      },
+      pushChanges: async ({ changes }) => {
+        const user = changes.users;
+        await api.post('/users/sync', user).catch(console.log);
+      },
+    });
+  }
 
   useEffect(()=>{
     let isMounted = true;
 
     const fetchCars = async () => {
       try{
-        const response = await api.get('/cars');
+        const carColletion = database.get<ModelCar>('cars');
+        const cars = await carColletion.query().fetch();
 
         if(isMounted){
-          setCars(response.data);
+          setCars(cars);
         };
 
       }catch(error){
@@ -60,11 +79,10 @@ export function Home(){
   },[]);
 
   useEffect(() => {
-    if(netInfo.isConnected){
-      Alert.alert('Você está On-Line');
-    }else{
-      Alert.alert('Você está Of-Line');
-    }
+    if (netInfo.isConnected === true) {
+      offlineSynchronize();
+    };
+
   },[netInfo.isConnected]);
 
   /** Component Animation Exemple
@@ -119,7 +137,7 @@ export function Home(){
             data={cars}
             keyExtractor={item => item.id}
             renderItem={({item}) =>
-              <Car data={item} onPress={()=>handleCarDetails(item)}/>
+              <Car data={item} onPress={() => handleCarDetails(item.id)}/>
             }
           />
       }
